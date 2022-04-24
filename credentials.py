f101b81727234f821a5a89d716370c398c47ebf5
davine94 <davine94>
2022-04-24 20:00:09 +0300

    Created a class for the credentials

--- credentials.py
+++ credentials.py
@@ -1,2 +1,9 @@
 from usercredentials import User
 import random, string
+
+class Credentials:
+    """
+    Credentials class to create new credential instances
+    """
+
+    credentials_list = []
