2c0a8a62fb40ecff0cd5be3829f05c9b4347ba27
davine94 <davine94>
2022-04-24 20:32:35 +0300

    Used init to define the properties of new credentials

--- credentials.py
+++ credentials.py
@@ -3,7 +3,17 @@ import random, string
 
 class Credentials:
     """
-    Credentials class to create new credential instances
+    Credentials class to create new credentials 
     """
 
     credentials_list = []
+    
+    def __init__(self, site_name, user_name, password):
+        """
+        __init__ to define properties of new Credentials
+        Args:
+            site_name: Website in which the credentials belong to
+            user_name: username for website's account
+            password: password for said account
+        """
+
