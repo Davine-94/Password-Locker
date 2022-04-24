823f9a4f422db4cc938ce269d97c76f34fb5aeb0
davine94 <davine94>
2022-04-24 19:31:46 +0300

    Used the init method to define user properties

--- usercredentials.py
+++ usercredentials.py
@@ -4,4 +4,14 @@ class User:
     """
 
     user_list = []
+def __init__(self, first_name, sur_name, user_name, email, password):
+    
+        """
+        __init__ method to define properties for object User
+        Args:
+            first_name: New user first name.
+            sur_name: New user last name.
+            email: New user email address.
+            password: New user account password.
+        """
     
